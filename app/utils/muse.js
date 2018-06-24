79813632535fb08d7a9e87a6fd1ad9684eefb5b7
jdpigeon <morrisondano@gmail.com>
2018-06-24 19:40:19 -0400

    cant find Windows.winmd

--- app/utils/muse.js
+++ app/utils/muse.js
@@ -1,26 +1,31 @@
-
+const noble = require("noble-uwp");
+const bluetooth = require("bleat").webbluetooth;
+const { MUSE_SERVICE, MuseClient, zipSamples, channelNames } = require('muse-js');
 
 const initMuseClient = () => {
   return new MuseClient();
 };
 
-const createStream = async () => {
-  const client = initMuseClient();
-  console.log(client);
-  await client.connect();
-  client.controlResponses.subscribe(x => console.log("Response:", x));
-  await client.start();
-  console.log("Connected!");
+async function createStream() {
+  noble.startScanning();
+  // let device = await bluetooth.requestDevice({
+  //   filters: [{ services: [MUSE_SERVICE] }]
+  // });
+  // const gatt = await device.gatt.connect();
+  // const client = new MuseClient();
+  // await client.connect(gatt);
+  // await client.start();
+  // // Now do whatever with muse client...
 
-  client.eegReadings.subscribe(reading => {
-    console.log(reading);
-  });
-  client.telemetryData.subscribe(telemetry => {
-    console.log(telemetry);
-  });
-  client.accelerometerData.subscribe(acceleration => {
-    console.log(acceleration);
-  });
-};
+  // client.eegReadings.subscribe(reading => {
+  //   console.log(reading);
+  // });
+  // client.telemetryData.subscribe(telemetry => {
+  //   console.log(telemetry);
+  // });
+  // client.accelerometerData.subscribe(acceleration => {
+  //   console.log(acceleration);
+  // });
+}
 
 module.exports = { createStream };
