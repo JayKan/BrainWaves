25223f8eb073d91152afa7945dfd2393c90a2b24
jdpigeon <morrisondano@gmail.com>
2018-06-24 21:02:14 -0400

    libusb error not supported

--- app/utils/muse.js
+++ app/utils/muse.js
@@ -1,31 +1,24 @@
 const noble = require("noble-uwp");
 const bluetooth = require("bleat").webbluetooth;
-const { MUSE_SERVICE, MuseClient, zipSamples, channelNames } = require('muse-js');
+const { MuseClient, MUSE_SERVICE } = require("muse-js");
 
-const initMuseClient = () => {
-  return new MuseClient();
+const createStream = () => {
+  async function connect() {
+    let device = await bluetooth.requestDevice({
+      filters: [{ services: [MUSE_SERVICE] }]
+    });
+    const gatt = await device.gatt.connect();
+    const client = new MuseClient();
+    await client.connect(gatt);
+    await client.start();
+    // Now do whatever with muse client...
+  }
+
+  noble.on("stateChange", state => {
+    if (state === "poweredOn") {
+      connect();
+    }
+  });
 };
-
-async function createStream() {
-  noble.startScanning();
-  // let device = await bluetooth.requestDevice({
-  //   filters: [{ services: [MUSE_SERVICE] }]
-  // });
-  // const gatt = await device.gatt.connect();
-  // const client = new MuseClient();
-  // await client.connect(gatt);
-  // await client.start();
-  // // Now do whatever with muse client...
-
-  // client.eegReadings.subscribe(reading => {
-  //   console.log(reading);
-  // });
-  // client.telemetryData.subscribe(telemetry => {
-  //   console.log(telemetry);
-  // });
-  // client.accelerometerData.subscribe(acceleration => {
-  //   console.log(acceleration);
-  // });
-}
 
 module.exports = { createStream };
