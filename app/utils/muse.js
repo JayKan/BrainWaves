64c17116c0d96a92bc6c27f43e6521296f0774bf
jdpigeon <morrisondano@gmail.com>
2018-06-25 13:03:52 -0400

    Replacing node imports in node_modules; UUID filtering bugs

--- app/utils/muse.js
+++ app/utils/muse.js
@@ -1,21 +1,28 @@
 const noble = require("noble-uwp");
-const bluetooth = require("bleat").webbluetooth;
+const bleat = require('bleat').webbluetooth;
 const { MuseClient, MUSE_SERVICE } = require("muse-js");
 
 const createStream = () => {
   async function connect() {
-    let device = await bluetooth.requestDevice({
+    console.log(bleat);
+    console.log(MUSE_SERVICE)
+    let device = await bleat.requestDevice({
       filters: [{ services: [MUSE_SERVICE] }]
     });
+    console.log(device);
     const gatt = await device.gatt.connect();
+    console.log('Device name:', gatt.device.name);
     const client = new MuseClient();
     await client.connect(gatt);
     await client.start();
+    console.log('Connected!')
+    return client
     // Now do whatever with muse client...
   }
 
   noble.on("stateChange", state => {
     if (state === "poweredOn") {
+      console.log("powered on");
       connect();
     }
   });
